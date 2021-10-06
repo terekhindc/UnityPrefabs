a53e10af1524782b8a118f0e8018d9bef39023c3
VR CORP <head@vrcorp.ru>
2021-10-06 14:56:17 +0700

    Added 2D graph

---
+++ Graph.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Vector2[] coordinates;
+    public float pointSize = 0.3f;
+    
+    private void Start()
+    {
+        foreach (Vector2 coordinate in coordinates)
+        {
+            GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            go.transform.position = coordinate;
+            go.transform.localScale = Vector3.one * pointSize;
+        }
+    }
+}
